88c8be2416866e7a66ed5484369c57486bac5860
ZacharyJia <jia199474@gmail.com>
2019-09-23 17:31:04 +0800

    更新License，更新版本

--- src/extension.ts
+++ src/extension.ts
@@ -105,7 +105,6 @@ function registerSmartBackspace() {
 export function activate(context: ExtensionContext) {
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "hungry-delete" is now active!');
 
     context.subscriptions.push(registerSmartBackspace());
 
